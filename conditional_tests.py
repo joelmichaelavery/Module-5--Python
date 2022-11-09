0bb8396162a2d4ee7d67ff30cb995e51b873f4d1
joelmichaelavery <110940754+joelmichaelavery@users.noreply.github.com>
2022-11-08 18:42:04 -0600

    adding conditional_tests.py

---
+++ conditional_tests.py
@@ -0,0 +1,12 @@
+#Joel Avery
+#11/8/22
+#writes a series of conditional tests
+
+car = 'subaru' #car is initialized to subaru
+print("Is car == 'subaru'? I predict TRUE.")
+#checks if car is a subaru output should be TRUE
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict FALSE.")
+#checks if car is an audi output should be FALSE. 
+print(car == 'audi')
